dd0026f5b4c6ea10d1df6c1f978840688ad2829d
DigiSeikluseFan <63068008+DigiSeikluseFan@users.noreply.github.com>
2020-05-06 00:44:20 +0300

    Add files via upload

---
+++ game/StompDetector.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+var enemyDead = false
+var color=.7
+
+func _physics_process(_delta):
+	var bodies= get_overlapping_bodies()
+	for body in bodies:
+		if body.name=="Player":
+			get_parent().queue_free()
